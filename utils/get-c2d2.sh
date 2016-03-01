3b4276ab7137dfa507c8d2ca25a33e2740af0b05
C222 <c222streams@gmail.com>
2016-02-29 21:36:37 -0800

    Changing unzip move flags, adding prints

--- utils/get-c2d2.sh
+++ utils/get-c2d2.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+echo "Making sure PIP exists"
 wget -qO - https://bootstrap.pypa.io/get-pip.py | python -
 python get-pip.py
 rm ./get-pip.py
-
 pip install -U pip
 
+echo "Installing Python dependancies"
 pip install Flask requests urllib3 websocket-client
 
+exho "Installing C2D2"
 wget https://github.com/C222/C2D2/archive/master.zip -O .temp.zip
 unzip -u .temp.zip "C2D2-master/src/*" -d "./"
-mv -ufn ./C2D2-master/src/* ./
+mv -f ./C2D2-master/src/* ./
 rm -rf ./C2D2-master
 rm ./.temp.zip
-
 chmod +x c2d2.py
 
+echo "Editing configuration files"
 mv -n ./config.py.CHANGEME ./config.py
 edit ./config.py
 
